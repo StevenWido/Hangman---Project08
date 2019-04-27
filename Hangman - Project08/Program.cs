394efaafd196b27dceadbb0632af3a8221a2c24b
Steven Wido <stevenwido.dev@gmail.com>
2019-04-27 11:42:09 -0500

    Add project files.

---
+++ Hangman - Project08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman___Project08
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
